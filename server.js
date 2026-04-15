be1b10b97353b9c1dab40ad275670abe1e57fdc1
mitchshop87-cpu <mitchshop87@gmail.com>
2026-04-16 00:43:42 +0100

    Update server.js

--- server.js
+++ server.js
@@ -62,6 +62,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Nexus Social running on port ${PORT}`);
 });
